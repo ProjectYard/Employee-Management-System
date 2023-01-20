053b662bf25a91905a1c1653b49e0482346ca661
Ashish Sahu <ashishsahu10428@gmail.com>
2023-01-21 02:06:19 +0530

    Model Added 🔁

---
+++ EMS-Server/EMS/EMS/Models/Domain/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace EMS.Models.Domain
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Salary { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
