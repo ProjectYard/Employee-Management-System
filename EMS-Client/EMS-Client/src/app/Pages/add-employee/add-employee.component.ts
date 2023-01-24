b0b4c568e8ed40198e651d94b97cd8819aad8445
Ashish Sahu <ashishsahu10428@gmail.com>
2023-01-25 04:18:45 +0530

    POST method success

--- EMS-Client/EMS-Client/src/app/Pages/add-employee/add-employee.component.ts
+++ EMS-Client/EMS-Client/src/app/Pages/add-employee/add-employee.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,8 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AddEmployeeComponent {
   emp= {
-    fname:"",
-    lname:"",
+    firstName:"",
+    lastName:"",
     phone:"",
     email:"",
     salary:0,
@@ -17,7 +18,35 @@ export class AddEmployeeComponent {
     password:""
   };
 
+  constructor(private http: HttpClient) { 
+    
+  }
+
+  clearObj(){
+    this.emp.firstName="",
+    this.emp.lastName="",
+    this.emp.phone="",
+    this.emp.email="",
+    this.emp.salary=0,
+    this.emp.location="",
+    this.emp.department="",
+    this.emp.password=""
+  }
+
+  postSuccess:boolean = false;
+
   submitEmployee(){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: 'my-auth-token'
+      })
+    }
     console.log(this.emp);
+    this.http.post("https://localhost:44393/api/Employee",this.emp,httpOptions).subscribe((data)=>{
+      console.log(data);
+      this.postSuccess = true;
+    });
+    this.clearObj();
   }
 }
